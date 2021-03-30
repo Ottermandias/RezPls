cc86d83a71a12eea8bfb855fb913c6fed0077411
Ottermandias <nickroederspam+git1@gmail.com>
2021-03-30 20:06:56 +0200

    Use hudagent for managing rects.

---
+++ HudManager.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using Dalamud.Game;
+using Dalamud.Hooking;
+using Dalamud.Plugin;
+using RezzPls.SeFunctions;
+
+namespace RezzPls
+{
+    public unsafe class HudManager : IDisposable
+    {
+        private const int GroupMemberOffset    = 0x0B50;
+        private const int AllianceMemberOffset = 0x0C4C;
+        private const int AllianceSizeOffset   = 0x0CEC;
+        private const int GroupMemberSize      = 0x18;
+        private const int GroupMemberIdOffset  = 0x10;
+
+        private readonly Hook<UpdatePartyDelegate> _updatePartyHook;
+        private          IntPtr                    _hudAgentPtr = IntPtr.Zero;
+
+        public HudManager(SigScanner scanner)
+        {
+            UpdateParty updateParty = new(scanner);
+            _updatePartyHook = updateParty.CreateHook(UpdatePartyHook, this)!;
+        }
+
+        private readonly int[,] _idOffsets =
+        {
+            {
+                GroupMemberOffset + 0 * GroupMemberSize + GroupMemberIdOffset,
+                GroupMemberOffset + 1 * GroupMemberSize + GroupMemberIdOffset,
+                GroupMemberOffset + 2 * GroupMemberSize + GroupMemberIdOffset,
+                GroupMemberOffset + 3 * GroupMemberSize + GroupMemberIdOffset,
+                GroupMemberOffset + 4 * GroupMemberSize + GroupMemberIdOffset,
+                GroupMemberOffset + 5 * GroupMemberSize + GroupMemberIdOffset,
+                GroupMemberOffset + 6 * GroupMemberSize + GroupMemberIdOffset,
+                GroupMemberOffset + 7 * GroupMemberSize + GroupMemberIdOffset,
+
+                AllianceMemberOffset + 0 * 4,
+                AllianceMemberOffset + 1 * 4,
+                AllianceMemberOffset + 2 * 4,
+                AllianceMemberOffset + 3 * 4,
+                AllianceMemberOffset + 4 * 4,
+                AllianceMemberOffset + 5 * 4,
+                AllianceMemberOffset + 6 * 4,
+                AllianceMemberOffset + 7 * 4,
+                AllianceMemberOffset + 8 * 4,
+                AllianceMemberOffset + 9 * 4,
+                AllianceMemberOffset + 10 * 4,
+                AllianceMemberOffset + 11 * 4,
+                AllianceMemberOffset + 12 * 4,
+                AllianceMemberOffset + 13 * 4,
+                AllianceMemberOffset + 14 * 4,
+                AllianceMemberOffset + 15 * 4,
+            },
+            {
+                GroupMemberOffset + 0 * GroupMemberSize + GroupMemberIdOffset,
+                GroupMemberOffset + 1 * GroupMemberSize + GroupMemberIdOffset,
+                GroupMemberOffset + 2 * GroupMemberSize + GroupMemberIdOffset,
+                GroupMemberOffset + 3 * GroupMemberSize + GroupMemberIdOffset,
+
+                AllianceMemberOffset + 0 * 4,
+                AllianceMemberOffset + 1 * 4,
+                AllianceMemberOffset + 2 * 4,
+                AllianceMemberOffset + 3 * 4,
+                AllianceMemberOffset + 8 * 4,
+                AllianceMemberOffset + 9 * 4,
+                AllianceMemberOffset + 10 * 4,
+                AllianceMemberOffset + 11 * 4,
+                AllianceMemberOffset + 16 * 4,
+                AllianceMemberOffset + 17 * 4,
+                AllianceMemberOffset + 18 * 4,
+                AllianceMemberOffset + 19 * 4,
+                AllianceMemberOffset + 24 * 4,
+                AllianceMemberOffset + 25 * 4,
+                AllianceMemberOffset + 26 * 4,
+                AllianceMemberOffset + 27 * 4,
+                AllianceMemberOffset + 32 * 4,
+                AllianceMemberOffset + 33 * 4,
+                AllianceMemberOffset + 34 * 4,
+                AllianceMemberOffset + 35 * 4,
+            },
+        };
+
+        public int GroupSize
+            => *(int*) (_hudAgentPtr + AllianceSizeOffset);
+
+        public bool IsAlliance
+            => GroupSize == 8;
+
+        public bool IsPvP
+            => GroupSize == 4;
+
+        public bool IsGroup
+            => GroupSize == 0;
+
+        public (int groupIdx, int idx)? FindGroupMemberById(uint actorId)
+        {
+            if (_hudAgentPtr == IntPtr.Zero)
+                return null;
+
+            var groupSize = GroupSize;
+            int numGroups;
+            if (groupSize == 0)
+            {
+                numGroups = 1;
+                groupSize = 8;
+            }
+            else
+            {
+                numGroups = groupSize == 4 ? 6 : 3;
+            }
+
+            var count = numGroups * groupSize;
+            var pvp   = groupSize == 4 ? 1 : 0;
+            for (var i = 0; i < count; ++i)
+            {
+                if (*(uint*) (_hudAgentPtr + _idOffsets[pvp, i]) == actorId)
+                    return (i / groupSize, i % groupSize);
+            }
+
+            return null;
+        }
+
+        private void UpdatePartyHook(IntPtr hudAgent)
+        {
+            _hudAgentPtr = hudAgent;
+            PluginLog.LogVerbose($"Obtained HUD agent at address 0x{_hudAgentPtr.ToInt64():X16}.");
+            _updatePartyHook.Original(hudAgent);
+            _updatePartyHook.Disable();
+            _updatePartyHook.Dispose();
+        }
+
+        public void Dispose()
+        {
+            _updatePartyHook?.Dispose();
+        }
+    }
+}
--- Interface.cs
+++ Interface.cs
@@ -1,28 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Numerics;
-using System.Reflection;
 using System.Runtime.InteropServices;
-using System.Runtime.Remoting.Messaging;
-using System.Windows.Forms.ComponentModel.Com2Interop;
 using Dalamud.Game.ClientState.Actors.Types;
-using Dalamud.Hooking;
 using Dalamud.Plugin;
-using FFXIVClientStructs;
 using FFXIVClientStructs.Component.GUI;
-using FFXIVClientStructs.Group;
 using ImGuiNET;
-using RezzPls.SeFunctions;
 
 namespace RezzPls
 {
     public class Interface : IDisposable
     {
-        private readonly DalamudPluginInterface    _pluginInterface;
-        private readonly Hook<UpdatePartyDelegate> _updatePartyHook;
-        private          IntPtr                    _hudAgentPtr = IntPtr.Zero;
+        private readonly DalamudPluginInterface _pluginInterface;
+        private readonly HudManager             _hudManager;
 
         private readonly IReadOnlyDictionary<int, (Actor? caster, Actor? target)> _rezzes;
         private readonly IReadOnlyDictionary<int, Actor>                          _rezzed;
@@ -37,31 +28,20 @@ namespace RezzPls
             _rezzes                              =  actorWatcher.RezzList;
             _rezzed                              =  actorWatcher.RezzedList;
             _pluginInterface.UiBuilder.OnBuildUi += Draw;
-
-            UpdateParty updateParty = new(_pluginInterface.TargetModuleScanner);
-            _updatePartyHook = updateParty.CreateHook(UpdatePartyHook, this)!;
+            _hudManager                          =  new HudManager(_pluginInterface.TargetModuleScanner);
 
             _phoenixDown = _pluginInterface.UiBuilder.LoadImage(@"H:\Garfield.png");
         }
 
         public void Dispose()
         {
-            _updatePartyHook.Dispose();
+            _hudManager.Dispose();
             _pluginInterface.UiBuilder.OnBuildUi -= Draw;
             _phoenixDown.Dispose();
         }
-        
-        private void UpdatePartyHook(IntPtr hudAgent)
-        {
-            _hudAgentPtr = hudAgent;
-            PluginLog.LogVerbose($"Obtained HUD agent at address 0x{_hudAgentPtr.ToInt64():X16}.");
-            _updatePartyHook.Original(hudAgent);
-            _updatePartyHook.Disable();
-            _updatePartyHook.Dispose();
-        }
-
 
         private static readonly Vector2 IconSize = new(150, 200);
+
         public void DrawIcon(ImDrawListPtr drawPtr, SharpDX.Vector2 pos, string text = "UNDEADED")
         {
             ImGui.SetCursorPos(new Vector2(pos.X, pos.Y - IconSize.Y / 2) - IconSize / 2);
@@ -107,9 +87,9 @@ namespace RezzPls
             idx = 17 - idx;
             var nodePtr  = (AtkComponentNode*) partyList->ULDData.NodeList[idx];
             var colNode  = nodePtr->Component->ULDData.NodeList[2];
-            var rectMin  = GetNodePosition(colNode) + new Vector2(5 * colNode->ScaleX, 5 * colNode->ScaleY);
-            var rectSize = new Vector2(colNode->Width, colNode->Height) - new Vector2(8 * colNode->ScaleX, 10 * colNode->ScaleY);
-            drawPtr.AddRectFilled(rectMin, rectMin + rectSize, 0x4000FF00, 10);
+            var rectMin  = GetNodePosition(colNode) + new Vector2(5 * partyList->Scale, 5 * partyList->Scale);
+            var rectSize = new Vector2(colNode->Width * partyList->Scale, colNode->Height * partyList->Scale) - new Vector2(8 * partyList->Scale, 10 * partyList->Scale);
+            drawPtr.AddRectFilled(rectMin, rectMin + rectSize, 0x4000FF00, 10 * partyList->Scale);
             if (caster.Length <= 0)
                 return;
 
@@ -133,51 +113,6 @@ namespace RezzPls
             ImGui.Text(caster);
         }
 
-        private const int GroupManagerOffset = 0x1DDAA50;
-        private const int AllianceRaidOffset = 0x1180;
-        private const int PMemberIdOffset    = 0x01A8;
-        private const int MemberNameOffset   = 0x01C4;
-        private const int AMemberIdOffset    = AllianceRaidOffset + PMemberIdOffset;
-
-        // @formatter:off
-        private static readonly int[,] ActorOffsets = new int[2,24]
-        {
-            {
-                PMemberIdOffset +  0 * 0x230, PMemberIdOffset +  1 * 0x230, PMemberIdOffset +  2 * 0x230, PMemberIdOffset +  3 * 0x230, 
-                PMemberIdOffset +  4 * 0x230, PMemberIdOffset +  5 * 0x230, PMemberIdOffset +  6 * 0x230, PMemberIdOffset +  7 * 0x230,
-                AMemberIdOffset +  0 * 0x230, AMemberIdOffset +  1 * 0x230, AMemberIdOffset +  2 * 0x230, AMemberIdOffset +  3 * 0x230, 
-                AMemberIdOffset +  4 * 0x230, AMemberIdOffset +  5 * 0x230, AMemberIdOffset +  6 * 0x230, AMemberIdOffset +  7 * 0x230,
-                AMemberIdOffset +  8 * 0x230, AMemberIdOffset +  9 * 0x230, AMemberIdOffset + 10 * 0x230, AMemberIdOffset + 11 * 0x230, 
-                AMemberIdOffset + 12 * 0x230, AMemberIdOffset + 13 * 0x230, AMemberIdOffset + 14 * 0x230, AMemberIdOffset + 15 * 0x230,
-            },
-            {
-                PMemberIdOffset +  0 * 0x230, PMemberIdOffset +  1 * 0x230, PMemberIdOffset +  2 * 0x230, PMemberIdOffset +  3 * 0x230,
-                AMemberIdOffset +  0 * 0x230, AMemberIdOffset +  1 * 0x230, AMemberIdOffset +  2 * 0x230, AMemberIdOffset +  3 * 0x230,
-                AMemberIdOffset +  4 * 0x230, AMemberIdOffset +  5 * 0x230, AMemberIdOffset +  6 * 0x230, AMemberIdOffset +  7 * 0x230,
-                AMemberIdOffset +  8 * 0x230, AMemberIdOffset +  9 * 0x230, AMemberIdOffset + 10 * 0x230, AMemberIdOffset + 11 * 0x230,
-                AMemberIdOffset + 12 * 0x230, AMemberIdOffset + 13 * 0x230, AMemberIdOffset + 14 * 0x230, AMemberIdOffset + 15 * 0x230,
-                AMemberIdOffset + 16 * 0x230, AMemberIdOffset + 17 * 0x230, AMemberIdOffset + 18 * 0x230, AMemberIdOffset + 19 * 0x230,
-            },
-        };
-        // @formatter:on
-
-        private unsafe (int, int)? FindCorpseInGroup(Actor corpse)
-        {
-            var basePtr     = (GroupManager*) (_pluginInterface.TargetModuleScanner.Module.BaseAddress + GroupManagerOffset);
-            var pvpAlliance = (basePtr->Unk_3D60 & 0x2) == 0x2 ? 1 : 0;
-            var maxPlayers  = basePtr->IsAlliance ? 24 : 8;
-            for (var i = 0; i < maxPlayers; ++i)
-            {
-                var memberId = *(uint*) ((byte*) basePtr + ActorOffsets[pvpAlliance, i]);
-                if (memberId != corpse.ActorId)
-                    continue;
-
-                var groupSize = pvpAlliance == 1 ? 4 : 8;
-                return (i / groupSize, i % groupSize);
-            }
-
-            return null;
-        }
 
         public unsafe void DrawOnPartyFrames(ImDrawListPtr drawPtr)
         {
@@ -195,18 +130,21 @@ namespace RezzPls
             {
                 if (anyParty)
                 {
-                    var group = FindCorpseInGroup(corpse);
+                    var group = _hudManager.FindGroupMemberById((uint) corpse.ActorId);
                     if (group != null)
-                        switch (@group.Value.Item1)
+                        switch (group.Value.groupIdx)
                         {
                             case 0:
-                                DrawPartyRect(drawPtr, party, @group.Value.Item2, caster == null ? "" : caster.Name);
+                                if(drawParty)
+                                    DrawPartyRect(drawPtr, party, group.Value.idx, caster == null ? "" : caster.Name);
                                 break;
                             case 1:
-                                DrawAllianceRect(drawPtr, alliance1, @group.Value.Item2, caster == null ? "" : caster.Name);
+                                if(drawAlliance1)
+                                    DrawAllianceRect(drawPtr, alliance1, group.Value.idx, caster == null ? "" : caster.Name);
                                 break;
                             case 2:
-                                DrawAllianceRect(drawPtr, alliance2, @group.Value.Item2, caster == null ? "" : caster.Name);
+                                if(drawAlliance2)
+                                    DrawAllianceRect(drawPtr, alliance2, group.Value.idx, caster == null ? "" : caster.Name);
                                 break;
                         }
                 }
